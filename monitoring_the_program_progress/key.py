eea1644b34941df730164aad8da69d0dc0693576
Crumer <crumer@inbox.ru>
2021-05-24 11:19:33 +0300

    add

---
+++ monitoring_the_program_progress/key.py
@@ -0,0 +1,9 @@
+r1 = {
+	'ios': '15.4',
+	'ip': '10.253.0.1',
+	'hostname': 'london_r1',
+	'location': '21 New Globe Walk',
+	'model': '4451',
+	'vendor': 'Cisco'}
+for k in r1:
+	print (k+':'+ r1[k])
---
+++ monitoring_the_program_progress/range.py
@@ -0,0 +1,2 @@
+for i in range (49):
+	print('interface Fa0/{}'.format(i))
